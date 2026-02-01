0c422a8942d9339c8d57292ada433c0286c0deef
petrol <pietrogouvearepleop@gmail.com>
2026-02-01 00:39:48 -0300

    height and border fix

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -11,7 +11,7 @@
 
 <div class="flex min-h-screen max-w-screen flex-col items-center justify-center gap-2">
 	<img src="/pexe-concord.png" alt="" class="hidden xl:block xl:mb-10">
-	<div class="bg-[#393A41] w-full h-screen overflow-y-auto pt-4 pb-9 md:text-2xl xl:max-w-[800px] xl:max-h-[500px]">
+	<div class="bg-[#393A41] w-full h-screen overflow-y-auto pt-4 md:text-2xl xl:max-w-[800px] xl:max-h-[370px] xl:rounded-lg">
 		<img src="/pexe-concord.png" alt="" class="block max-w-[300px] mx-auto md:max-w-[500px] xl:hidden">
 		<div class="flex flex-col items-center mt-4 gap-3">
 			<span class="font-bold text-xl md:text-4xl lg:text-4xl">Welcome back freedom enjoyer!</span>
