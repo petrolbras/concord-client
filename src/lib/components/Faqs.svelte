2b255450ab13a54b08508cfa9d5b5f9eec17f9ae
petrol <pietrogouvearepleop@gmail.com>
2026-04-03 23:22:03 -0300

    Created About web page

---
+++ src/lib/components/Faqs.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+
+    let openIndex: number | null = null;
+
+    let faqs = [
+        {
+            question: "What is Concord?",
+            answer: "Concord is a distributed voice/chat application. It allows you to host your own voice/chat server, giving you full control over your data and privacy."
+        },
+        {
+            question: "How does Concord work?",
+            answer: "In Concord, every server can be independently hosted, there's no central authority. If you wish to create a server, you can choose to either host it yourself or pay for external hosting."
+        },
+        {
+            question: "Is concord open-source?",
+            answer: `Yes, Concord is open-source. You can find the source code on our <a href="https://github.com/liber-organiza-cao/concord-client" class="text-orange-500 hover:underline">GitHub repository</a> and contribute to the project if you wish.`
+        },
+        {
+            question: "Is Concord free to use?",
+            answer: `Yes, Concord is free for users. They can freely join public servers or host their own server with their own set of rules and invite others to join. However, hosting a server may involve costs, especially if you choose to use external hosting services.`
+        },
+        {
+            question: "Can I use Concord on my mobile device?",
+            answer: "Yes, however Concord currently doesn't have a mobile app, but this is planned for the future. At the moment, it can only be accessed through the browser."
+        },
+        {
+            question: "How do I set up my own Concord server?",
+            answer: "You can find more info about this topic in our <a href='https://github.com/liber-organiza-cao/concord-server' class='text-orange-500 hover:underline'>GitHub repository</a>."
+        }
+    ];
+
+    function toggle(index: number) {
+		openIndex = openIndex === index ? null : index;
+	}
+</script>
+
+<div class="max-w-2xl mx-auto p-4 mt-8 space-y-4">
+    <h1 class="items-center justify-center text-5xl font-extrabold text-center pb-6">
+        FAQs about Concord
+    </h1>
+
+	{#each faqs as faq, i}
+		<div class="border border-white/10 rounded-xl overflow-hidden bg-slate-900">
+			
+			<button onclick={() => toggle(i)} class="w-full flex justify-between items-center p-4 text-left">
+			    <span class="font-medium">{faq.question}</span>
+				
+			    <span class="transition-transform duration-200 text-1xl ease-in-out {openIndex === i ? 'rotate-180' : ''}">
+				    <i class="fa-solid fa-angle-up"></i>
+			    </span>
+			</button>
+
+			{#if openIndex === i}
+				<div class="p-4 pt-4 text-md text-gray-300 border-t border-white/10">
+					{@html faq.answer}
+				</div>
+			{/if}
+        </div>
+	{/each}
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+    import Faqs from "$lib/components/Faqs.svelte";
+    import Footer from "$lib/components/footer.svelte";
+	import HamburgerMenu from "$lib/components/hamburgerMenu.svelte";
+</script>
+
+<div class="min-h-screen flex flex-col overflow-hidden">
+    <header>
+        <div class="flex items-center justify-center gap-4 p-4 bg-slate-900 border-b ">
+            <button class="flex items-center gap-2">
+                <img src="/pexe.png" alt="pexe" class="h-12 w-12"/>
+                <h1 class="text-2xl font-bold mt-2">Concord</h1>
+            </button>
+
+            <div class="hidden md:block absolute left-1/2 -translate-x-1/2 flex gap-5">
+                <a href="/concord" class="text-lg inline-block hover:scale-105 rounded-md p-2">Main page</a>
+                <a href="/dev" class="text-lg inline-block hover:scale-105  rounded-md p-2">Developers</a>
+                <a href="https://github.com/liber-organiza-cao" class="text-lg inline-block hover:scale-105  rounded-md p-2">GitHub</a>
+            </div>
+
+		    <a
+			    href="https://alvorada.site/"
+			    target="_blank"
+		    	class="ml-auto bg-orange-500 hover:bg-orange-400 text-center rounded-2xl p-2"
+		    >
+			    Open Concord
+		    </a>
+
+            <HamburgerMenu links={[
+                { href: "/concord", label: "Main" },
+                { href: "/dev", label: "Developers" }
+            ]} />
+
+        </div>
+    </header>
+    <main class="flex-1">
+        
+        <Faqs />
+
+        <div class="flex flex-col items-center justify-center gap-8 p-8">
+            <h1 class="text-2xl md:text-4xl font-extrabold text-center">
+                Your private voice chat server
+            </h1>
+
+            <p class="text-md md:text-lg text-gray-200 max-w-4xl text-center">
+                Concord is perfect for those who prioritize privacy and personal freedom. Unlike large companies, we don't rely on centralized servers, as we are self-hosted. Moreover, you don't have to sacrifice your data just to use our services.
+            </p>
+        </div>
+
+        <div class="flex items-center justify-center p-4 mb-12">
+            <a href="https://alvorada.site/" target="_blank" class="bg-orange-500 hover:bg-orange-400 text-xl p-4 rounded-3xl">
+                Open Concord in your browser
+        </div>
+    </main>
+    <footer>
+        <Footer />
+    </footer>
+</div>
